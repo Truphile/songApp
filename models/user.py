fe57d9bf93d29b7a851c2ba99bc22e2554f10953
Truphile <salidmonreal@gmail.com>
2026-02-23 20:28:57 +0100

    new commit

---
+++ models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from repositories.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    password = Column(String(255), nullable=False)
+
+    role = Column(String(50), default="user")
+
+    songs = relationship("Song", back_populates="owner")
