61e854be5eefa4fbb672091a858f3a0c6994634c
Truphile <salidmonreal@gmail.com>
2026-02-24 02:18:03 +0100

    new commit

---
+++ repositories/authDependency.py
@@ -0,0 +1,16 @@
+from fastapi import Header, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from models.user import User
+from repositories.database import get_db
+
+
+def get_current_user():
+    email: str = Header(...)
+    database: Session = Depends(get_db())
+
+    user = database.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=401,detail="UNAUTHORIZED")
+    return user
