c908be3dcc85ec9be94072d271b0f41eaef27ce7
Truphile <salidmonreal@gmail.com>
2026-02-24 03:13:05 +0100

    new commit

--- routers/songRouter.py
+++ routers/songRouter.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models.user import User
 from repositories.authDependency import get_current_user
 from repositories.database import get_db
-from schemas.songSchemas import createSong
-from services.songService import create_song, search_songs
+from schemas.songSchemas import createSong, deleteSong
+from services.songService import create_song, search_songs, delete_song
 
 router = APIRouter(prefix="/songs")
 
@@ -17,4 +17,9 @@ def search_song(query: str, database: Session = Depends(get_db),current_user: Us
     return search_songs(database, query)
 
 @router.delete("/{song_id}")
-def remove_song
+def remove_song(song_id: int, database: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+    deleted_song = delete_song(database,song_id,current_user)
+
+    if not deleted_song:
+        raise HTTPException(status_code=404, detail="Song not found")
+    return {"message": "Song deleted successfully"}
