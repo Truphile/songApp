92e1b17df1db841eaeb58b0fd4e436b49a914080
Truphile <salidmonreal@gmail.com>
2026-02-24 02:32:53 +0100

    new commit

---
+++ services/songService.py
@@ -0,0 +1,20 @@
+from models.song import Song
+
+
+def create_song(database, song_data, current_user):
+    new_song = Song(
+        title=song_data.title,
+        artist=song_data.artist,
+        owner_id=current_user.id
+    )
+
+    database.add(new_song)
+    database.commit()
+    database.refresh(new_song)
+
+    return new_song
+
+def update_song(database, song_id, song_data, current_user):
+
+    if current_user.role == "admin":
+        song = database.query(Song).filter(Song.id == song_id).first()
