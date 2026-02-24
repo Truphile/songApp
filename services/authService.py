e51fcbf337d116b71530da4b90b30dbd99f0ead2
Truphile <salidmonreal@gmail.com>
2026-02-24 02:45:08 +0100

    new commit

--- services/authService.py
+++ services/authService.py
@@ -1,1 +1,24 @@
-def create_s
+import hashlib
+
+from models.user import User
+
+
+def hash_password(password: str):
+    return hashlib.sha512(password.encode()).hexdigest()
+
+def verify_password(password: str, hashed_password: str):
+    return hashed_password == hashlib.sha512(password.encode()).hexdigest()
+
+def register_user(database, user_data):
+
+    new_user = User(
+    password = hash_password(user_data.password),
+    email = user_data.email,
+    role = "user"
+    )
+
+    database.add(new_user)
+    database.commit()
+    database.refresh(new_user)
+
+    return new_user
