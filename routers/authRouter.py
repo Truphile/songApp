3d2eb068d41c0064c64314de6a964ea482cf3559
Truphile <salidmonreal@gmail.com>
2026-02-24 02:53:23 +0100

    new commit

---
+++ routers/authRouter.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from repositories.database import get_db
+from schemas.userSchemas import userRegister
+from services.authService import register_user
+
+router = APIRouter()
+
+@router.post("/register")
+def register(user: userRegister, database: Session = Depends(get_db())):
+    new_user = register_user(database, user)
+
+    if not new_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    return {"message": "User registered"}
+
+@router.post("/login")
