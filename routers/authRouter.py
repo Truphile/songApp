3f12f07e2e49783bf621a254bc1ca725eabe5b89
Truphile <salidmonreal@gmail.com>
2026-02-24 02:53:48 +0100

    new commit

--- routers/authRouter.py
+++ routers/authRouter.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from repositories.database import get_db
-from schemas.userSchemas import userRegister
+from schemas.userSchemas import userRegister, userLogin
 from services.authService import register_user
 
 router = APIRouter()
 
 @router.post("/register")
-def register(user: userRegister, database: Session = Depends(get_db())):
+def register(user: userRegister, database: Session = Depends(get_db)):
     new_user = register_user(database, user)
 
     if not new_user:
@@ -17,3 +17,4 @@ def register(user: userRegister, database: Session = Depends(get_db())):
     return {"message": "User registered"}
 
 @router.post("/login")
+def login(user: userLogin, db: Session = Depends(get_db)):
